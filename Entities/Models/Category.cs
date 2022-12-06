d74057b471025c0c99bab8915347ba8bf16a2687
Amit Singh <np4652@gmail.com>
2022-12-06 12:09:14 +0530

    done

--- Entities/Models/Category.cs
+++ Entities/Models/Category.cs
@@ -10,7 +10,6 @@ namespace Entities.Models
         public int ParentId { get; set; }
         public string ParentName { get; set; }
         public bool IsPublish { get; set; }
-        [Required(ErrorMessage = "Please Select Icon")]
         public string Icon { get; set; }
     }
     public class CategoryDDL
