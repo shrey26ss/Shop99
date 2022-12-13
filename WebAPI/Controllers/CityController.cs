6e7d18a127dcaa31d5a6d9acdd3a4171d570973a
Roundpay Developer Akash <Roundpay Developer@DESKTOP-3FMP3QI>
2022-12-13 18:41:55 +0530

    Done

--- WebAPI/Controllers/CityController.cs
+++ WebAPI/Controllers/CityController.cs
@@ -8,21 +8,19 @@ using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
 {
+    [Authorize(AuthenticationSchemes = "Bearer")]
     [ApiController]
     [Route("/api/")]
     public class CityController : ControllerBase
     {
         public readonly ICity _city;
         public CityController(ICity city) => _city = city;
-        [Authorize(AuthenticationSchemes = "Bearer")]
         [Route("City/AddUpdate")]
         [ProducesResponseType(typeof(Response), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> AddUpdate(Cities req) => Ok(await _city.AddUpdate(req));
-        [Authorize(AuthenticationSchemes = "Bearer")]
         [Route("City/ChangeStatus")]
         [ProducesResponseType(typeof(Response), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> ChangeStatus(SearchItem req) => Ok(await _city.ChangeStatus(req));
-        [Authorize(AuthenticationSchemes = "Bearer")]
         [Route("City/GetCities")]
         public async Task<IActionResult> GetCities() => Ok(await _city.GetList());
         [AllowAnonymous]
--- WebAPI/Controllers/StateController.cs
+++ WebAPI/Controllers/StateController.cs
@@ -7,22 +7,20 @@ using System.Net;
 using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
-{    
+{
+    [Authorize(AuthenticationSchemes = "Bearer")]
     [ApiController]
     [Route("/api/")]
     public class StateController : ControllerBase
     {
         public readonly IStates _states;
         public StateController(IStates states) => _states = states;
-        [Authorize(AuthenticationSchemes = "Bearer")]
         [Route("State/AddUpdate")]
         [ProducesResponseType(typeof(Response), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> AddUpdate(States req) => Ok(await _states.AddUpdate(req));
-        [Authorize(AuthenticationSchemes = "Bearer")]
         [Route("State/ChangeStatus")]
         [ProducesResponseType(typeof(Response), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> ChangeStatus(SearchItem req) => Ok(await _states.ChangeStatus(req));
-        [Authorize(AuthenticationSchemes = "Bearer")]
         [Route("State/GetSates")]
         public async Task<IActionResult> GetSates() => Ok(await _states.GetList());
         [AllowAnonymous]
