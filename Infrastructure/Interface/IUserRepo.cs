dc69b1ece8ed548ec4b32e079a37aa6fdd1bfdb8
Round Pay <Round Pay@DESKTOP-4LKB5TE>
2022-12-22 10:56:29 +0530

    22_12_2022

---
+++ Infrastructure/Interface/IUserRepo.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Enums;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Interface
+{
+    public interface IUserRepo 
+    {
+        Task<IResponse<IEnumerable<UserDetails>>> GetUserListByRole(Role id);
+    }
+}
---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Models;
+using Infrastructure.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebAPI.Middleware;
+
+namespace WebAPI.Controllers
+{
+    [Authorize(AuthenticationSchemes = "Bearer")]
+    [ApiController]
+    [Route("/api/")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepo _userservice;
+
+        public UserController(IUserRepo userservice) => _userservice = userservice;
+        [Route("User/CustomerList")]
+        public async Task<IActionResult> CustomerList() => Ok(await _userservice.GetUserListByRole(Entities.Enums.Role.Customer));
+        [Route("User/VendorList")]
+        public async Task<IActionResult> VendorList() => Ok(await _userservice.GetUserListByRole(Entities.Enums.Role.Vendor));
+    }
+}
