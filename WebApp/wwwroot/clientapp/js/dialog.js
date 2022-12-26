91567e1031deb7cf1e46198d85a94d9bf010ca37
Amit Singh <np4652@gmail.com>
2022-12-26 18:18:47 +0530

    done

---
+++ WebApp/wwwroot/clientapp/js/dialog.js
@@ -0,0 +1,21 @@
+﻿var dialog;
+((dialog) => {
+    dialog.category = function (Id) {
+        $.get('/Category/Create', { Id: Id })
+            .done(result => {
+                Q.alert({
+                    title: 'Category',
+                    body: result
+                });
+            }).fail(xhr => Q.renderError(xhr)).always(() => Q.preloader.remove());
+    };
+    dialog.brand = function (Id) {
+        $.get('/Brand/Create', { Id: Id })
+            .done(result => {
+                Q.alert({
+                    title: 'Brand',
+                    body: result
+                });
+            }).fail(xhr => Q.renderError(xhr)).always(() => Q.preloader.remove());
+    };
+})(dialog || (dialog = {}));
