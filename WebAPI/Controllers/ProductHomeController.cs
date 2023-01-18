fd41cad0383fb5dade4e7c33d65f03d0575f2ff2
Rishabh Goswami <rishabhgoswami143@gmail.com>
2023-01-18 18:48:38 +0530

    d

--- WebAPI/Controllers/ProductHomeController.cs
+++ WebAPI/Controllers/ProductHomeController.cs
@@ -9,29 +9,36 @@ namespace WebAPI.Controllers
     [AllowAnonymous]
     [ApiController]
     [Route("/api/")]
-    [ApiExplorerSettings(IgnoreApi = true)]
+   
     public class ProductHomeController : Controller
     {
         private readonly IProductHomeService _product;
 
         public ProductHomeController(IProductHomeService product) => _product = product;
 
+        [HttpPost]
         [Route("ProductHome/GetProductDetails")]
         public async Task<IActionResult> GetProductDetails(SearchItem req) => Ok(await _product.GetProductDetails(req));
+        [HttpPost]
         [Route("ProductHome/GetProductAllDetails")]
         public async Task<IActionResult> GetProductAllDetails(SearchItem req) => Ok(await _product.GetProductAllDetails(req));
 
+        [HttpPost]
         [Route("ProductHome/GetProductAttrDetails")]
         public async Task<IActionResult> GetProductAttrDetails(SearchItem req) => Ok(await _product.GetProductAttrDetails(req));
+        [HttpPost]
         [Route("ProductHome/GetProductAttributeInfo")]
         public async Task<IActionResult> GetProductAttributeInfo(SearchItem req) => Ok(await _product.GetProductAttributeInfo(req));
+        [HttpPost]
         [Route("ProductHome/GetProductPicDetails")]
         public async Task<IActionResult> GetProductPicDetails(SearchItem req) => Ok(await _product.GetProductPicDetails(req));
-
+        [HttpPost]
         [Route("ProductHome/GetVariantIdByAttributes")]
         public async Task<IActionResult> GetVariantIdByAttributes(VariantIdByAttributesRequest req) => Ok(await _product.GetVariantIdByAttributes(new RequestBase<VariantIdByAttributesRequest> { Data=req }));
+        [HttpPost]
         [Route("ProductHome/GetVariantDetailsByAttributes")]
         public async Task<IActionResult> GetVariantDetailsByAttributes(VariantIdByAttributesRequest req) => Ok(await _product.GetVariantdetailsByAttributes(new RequestBase<VariantIdByAttributesRequest> { Data = req }));
+        [HttpPost]
         [Route("ProductHome/GetVariantPicture")]
         public async Task<IActionResult> GetVariantPicture(VariantIdByAttributesRequest req) => Ok(await _product.PictureInformation(new RequestBase<VariantIdByAttributesRequest> { Data = req }));
     }
