6d5a065daed0c21bdf1fb5f664fe14d9b2bf9d65
Rishabh Goswami <rishabhgoswami143@gmail.com>
2023-02-07 10:51:43 +0530

    SatteAPi

--- WebAPI/Controllers/StateController.cs
+++ WebAPI/Controllers/StateController.cs
@@ -8,22 +8,27 @@ using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
 {
-    [ApiExplorerSettings(IgnoreApi = true)]
+   
     [Authorize(AuthenticationSchemes = "Bearer")]
     [ApiController]
     [Route("/api/")]
     public class StateController : ControllerBase
     {
         public readonly IStates _states;
+      
         public StateController(IStates states) => _states = states;
+        [ApiExplorerSettings(IgnoreApi = true)]
         [Route("State/AddUpdate")]
         [ProducesResponseType(typeof(Response), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> AddUpdate(States req) => Ok(await _states.AddUpdate(req));
+        [ApiExplorerSettings(IgnoreApi = true)]
         [Route("State/ChangeStatus")]
         [ProducesResponseType(typeof(Response), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> ChangeStatus(SearchItem req) => Ok(await _states.ChangeStatus(req));
+        [ApiExplorerSettings(IgnoreApi = true)]
         [Route("State/GetSates")]
         public async Task<IActionResult> GetSates() => Ok(await _states.GetList());
+        [HttpPost]
         [AllowAnonymous]
         [Route("State/GetStateDDL")]
         public async Task<IActionResult> GetStateDDL() => Ok(await _states.GetDDL());
