60fae9621af0802d6daa04100edfdd47e020e75b
Rishabh Goswami <rishabhgoswami143@gmail.com>
2023-02-13 18:25:55 +0530

    d

--- WebApp/wwwroot/clientapp/js/index.js
+++ WebApp/wwwroot/clientapp/js/index.js
@@ -2,15 +2,14 @@
     loadTopBrands();
     loadTopBannerSec();
     loadofferBanner();
-    loadNewProducts();
-    loadFeatureProducts();
-    loadBestSeller();
-    loadOnSale();
     loadHotDeals();
     loadHotDealsNewProduct();
     loadMainCategory();
-
+    loadTabs();    
 });
+
+
+
 const loadTopBrands = function () {
     let item = {
         OrderBy: 0,
@@ -237,9 +236,9 @@ const loadofferBanner = function () {
         }
     })
 }
-const loadNewProducts = async function () {
+const loadNewProducts = async function loadNewProducts () {
     await $.post("/ProductSection", { id: 1 }).done(res => {
-        $('#secDvProduct').html(res);
+        $('#secDvProduct').append(res);
     });
 }
 const loadFeatureProducts = async function () {
@@ -268,3 +267,112 @@ const loadHotDealsNewProduct = async function () {
         $('#dvHotDealNewProduct').append(res);
     });
 }
+
+const loadTabs = async () => {
+    $('#secDvProduct').html('');
+    await loadNewProducts();
+    await loadFeatureProducts();
+    await loadBestSeller();
+    await loadOnSale();
+
+    $('.media-slide-5').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 5,
+        centerPadding: '15px',
+        responsive: [
+            {
+                breakpoint: 1470,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 820,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true
+                }
+            }
+        ]
+    });
+
+    $('.tab-content.active.default').css("display", "block");
+    
+    $(".tabs li a").on('click', function () {
+        event.preventDefault();
+        //$('.tab_product_slider').slick('unslick');
+        //$('.media-slide-5').slick('unslick');
+        $(this).parent().parent().find("li").removeClass("current");
+        $(this).parent().addClass("current");
+        var currunt_href = $(this).attr("href");
+        $('#' + currunt_href).show();
+        console.log('currunt_href : ', currunt_href);
+        $(this).parent().parent().parent().find(".tab-content").not('#' + currunt_href).css("display", "none");
+        if ($('.media-slide-5').hasClass('slick-initialized')) {
+            $('.media-slide-5').slick('unslick')
+        }
+        $('.media-slide-5').slick({
+            dots: false,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 5,
+            centerPadding: '15px',
+            responsive: [
+                {
+                    breakpoint: 1470,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 4,
+                        infinite: true
+                    }
+                },
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true
+                    }
+                },
+                {
+                    breakpoint: 820,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        infinite: true
+                    }
+                },
+                {
+                    breakpoint: 576,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        infinite: true
+                    }
+                }
+            ]
+        });
+    });
+}
+    
