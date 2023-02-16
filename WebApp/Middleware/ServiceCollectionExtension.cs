bac37c6c4b04047c5461ffa67b67ec15ec4960d5
Amit Singh <np4652@gmail.com>
2023-02-16 16:29:24 +0530

    done

--- WebApp/Middleware/ServiceCollectionExtension.cs
+++ WebApp/Middleware/ServiceCollectionExtension.cs
@@ -1,17 +1,10 @@
 ﻿using Data;
 using Entities.Models;
-using Hangfire;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using NLog;
 using Services.Identity;
-using System.Collections.Generic;
-using System.Reflection;
-using Microsoft.OpenApi.Models;
-using System.IO;
 using Service.Identity;
-using FluentMigrator.Runner;
 using Infrastructure.Repos;
 using Infrastructure.Interface;
 using Service.Repos;
@@ -22,8 +15,6 @@ using WebApp.Servcie;
 using WebApp.AppCode.Helper;
 using AppUtility.Helper;
 using AppUtility.AppCode;
-using Microsoft.AspNetCore.Http;
-using Service.Websiteinfo;
 
 namespace WebApp.Middleware
 {
@@ -32,48 +23,31 @@ namespace WebApp.Middleware
         public static void RegisterService(this IServiceCollection services, IConfiguration configuration)
         {
             string dbConnectionString = configuration.GetConnectionString("SqlConnection");
-            GlobalDiagnosticsContext.Set("connectionString", dbConnectionString);
             IConnectionString ch = new ConnectionString { connectionString = dbConnectionString };
-
-            //var emailConfig = configuration.GetSection("EmailConfiguration").Get<EmailService.EmailConfiguration>();
-            //services.AddSingleton(emailConfig);
-            services.AddSingleton<IViewRenderService, ViewRenderService>();
-            //services.AddScoped<IEmailService, EmailFactory>();
-            services.AddSingleton<IConnectionString>(ch);
+            services.AddSingleton<IViewRenderService, ViewRenderService>();            
+            services.AddSingleton(ch);
             services.AddScoped<IDapperRepository, DapperRepository>();
             services.AddScoped<IHttpRequestInfo, HttpRequestInfo>();
             services.AddScoped<ApplicationDbContext>();
             services.AddScoped<IUserStore<ApplicationUser>, UserStore>();
             services.AddScoped<IRoleStore<ApplicationRole>, RoleStore>();
             services.AddScoped<IUserService, UserService>();
-            services.AddScoped<ICategoryAPI, WebApp.Servcie.CategoryAPI>();
+            services.AddScoped<ICategoryAPI, CategoryAPI>();
             services.AddScoped<ICheckOutAPI, CheckOutAPI>();
             services.AddScoped<ICartWishListAPI,CartWishListAPI>();
-            services.AddScoped<IProductsAPI, WebApp.Servcie.ProductsAPI>();
-            services.AddScoped<Infrastructure.Interface.ILog, LogNLog>();
+            services.AddScoped<IProductsAPI, ProductsAPI>();
+            services.AddScoped<ILog, LogNLog>();
             services.AddSingleton<ITokenService, TokenService>();
             services.AddSingleton<IDDLHelper, DDLHelper>();
             services.AddSingleton<IGenericMethods, GenericMethods>();
             services.AddScoped<Data.Migrations.Database>();
             //services.AddProgressiveWebApp();
             services.AddAutoMapper(typeof(Startup));
-            services.AddHangfire(x => x.UseSqlServerStorage(dbConnectionString));
-            services.AddHangfireServer();
-            services.AddLogging(c => c.AddFluentMigratorConsole())
-                .AddFluentMigratorCore()
-                .ConfigureRunner(c => c.AddSqlServer2016()
-                .WithGlobalConnectionString(configuration.GetConnectionString("SqlConnection"))
-                .ScanIn(Assembly.GetExecutingAssembly()).For.Migrations());
-            List<NotificationPermissions> notificationPermissions = new List<NotificationPermissions>();
-            configuration.GetSection("NotificationPermissions").Bind(notificationPermissions);
-            services.AddSingleton(notificationPermissions);
-            APIConfig apiConfig = new APIConfig();
-            configuration.GetSection("APIConfig").Bind(apiConfig);
-            services.AddSingleton(apiConfig);
-            services.AddSingleton(apiConfig);
-            JWTConfig jwtConfig = new JWTConfig();
-            configuration.GetSection("JWT").Bind(jwtConfig);
-            services.AddSingleton(jwtConfig);
+            
+            //JWTConfig jwtConfig = new JWTConfig();
+            //configuration.GetSection("JWT").Bind(jwtConfig);
+            //services.AddSingleton(jwtConfig);
+
             AppSettings appSettings = new AppSettings();
             configuration.Bind(appSettings);
             services.AddSingleton(appSettings);
